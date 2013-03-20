4a22b5a47ffc9544f070babfdfa031dbe6d73ea6
Zane M. Kolnik <zanematthew@gmail.com>
2013-03-20 13:56:22 -0400

    added latest function

--- functions.php
+++ functions.php
@@ -19,3 +19,24 @@ if ( ! get_option('zm_easy_cpt_version' ) ){
 require_once plugin_dir_path( __FILE__ ) . '../zm-easy-cpt/plugin.php';
 if ( ! function_exists( 'zm_easy_cpt_reqiure' ) ) return;
 zm_easy_cpt_reqiure( plugin_dir_path(__FILE__) );
+
+
+/**
+ * Displays most recently added plugins
+ */
+function zm_plugin_post_type_latest(){
+
+    $args = array(
+        'post_type' => 'plugins',
+        'numberposts' => 5,
+        );
+
+    $plugins = get_posts( $args );
+    ?>
+    <h4>Latest Plugins</h4>
+    <ul class="zm-plugin-post-type-latest">
+        <?php foreach( $plugins as $plugin ) : setup_postdata( $plugin ); ?>
+            <li><a href="<?php print get_permalink( $plugin->ID ); ?>" title="<?php print get_the_title( $plugin->ID ); ?>"><?php print get_the_title( $plugin->ID ); ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+<?php }
